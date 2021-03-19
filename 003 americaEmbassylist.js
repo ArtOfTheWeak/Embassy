701fbc8fcbe217816131f47daaf2aefcccfe3215
ArtOfTheWeak <80341310+ArtOfTheWeak@users.noreply.github.com>
2021-03-19 18:06:20 +0000

    Add files via upload

---
+++ 003 americaEmbassylist.js
@@ -0,0 +1,187 @@
+//A list of 156 US Embassies worldwide that operate a twitter account and their respective twitter IDs 
+// Based on wikipedia list of US Diplomatic missions: 
+//https://en.wikipedia.org/wiki/List_of_diplomatic_missions_of_the_United_States 
+//Twitter IDS from www.tweeterid.com
+//If you want to get handles, just put IDs into tweeterID.com
+
+
+
+// AFRICA 
+
+var algeria = '266008957'; 
+var benin = '2283571250';  
+var botswana = '481225816';
+var burkinaFaso = '275033412';
+var burundi = '727410381954523136';
+var cameroon = '1299489810';
+var capeVerde = '390106290';
+var cae = '4209001432';
+var repCongo = '2810124823';
+var demrepCongo = '61162525';
+var djibouti = '179832889';
+var egypt = '76945884'
+var equitorialGuinea = '756478622093967360';
+var swatini = '254140080';
+var ethiopia = '2438067822';
+var gabon = '90410353';
+var gambia = '732674730';
+var ghana = '217079025';
+var guinea = '145653919';
+var ivoryCoast = '1116262315';
+var kenya = '70636581';
+var lesotho = '250958881';
+var liberia = '289241101'; 
+var madagascar = '217403591';
+var malawi = '758303211925020673';
+var mali = '461359076';
+var mauritania = '1407934586';
+var mauritius = '1013319183432495104';
+var morocco = '468731306';
+var mozambique = '20593889';
+var namibia = '384760676';
+var niger = '2373370128';
+var rwanda = '2976268757';
+var senegal = '540820515';
+var sierraLeone = '1204811390';
+var somalia = '117795822';
+var sa = '33509142';
+var southSudan = '1729007461';
+var sudan = '218776584';
+var tanzania = '128534762';
+var togo = '340566737';
+var tunisia = '3028189937';
+var uganda = '197341208';
+var zambia = '18212352';
+var zimbabwe = '58451369';
+
+//AMERICAS\\\\\\
+
+var antiguaBarbadosGrenada = '163910493';
+var argentina = '486678517';
+var bahamas = '618334097';
+var belize = '2182811810';
+var bolivia = '168498034';
+var brazil = '24511456';
+var canada = '7001435';
+var chile = '102061279';
+var colombia = '85867670';
+var costaRica = '19738801';
+var cuba = '133769083';
+var dominicanRepublic = '35727265';
+var ecuador = '73050516';
+var salvador = '1364880882';
+var guatemala = '23664866';
+var guyana = '163550067';
+var haiti = '133379712';
+var honduras = '179563308';
+var jamaica = '2359516206';
+var mexico = '52528982';
+var nicaragua = '389075192';
+var panama = '77553367';
+var paraguay = '39803974';
+var peru = '92291383';
+var suriname = '3872875821';
+var trinidadTobago = '993611407';
+var uruguay = '117388701';
+var venezuela = '36414477';
+
+
+
+/////ASIA\\\\\\\\\\\\\\\\
+
+
+var afghanistan = '21645304';
+var armenia = '119023613';
+var azerbijan = '238019832';
+var bahrain = '262149951';
+var bangladesh = '16234513';
+var brunei = '2435051258';
+var cambodia = '167992419';
+var eastTimor = '141746127';
+var georgia = '250705901';
+var india = '41533816';
+var indonesia = '47279525';
+var iraq = '112356528';
+var israel = '44309594';
+var japan = '93471126';
+var jordan = '61442211';
+var kazakhstan = '216625028';
+var kuwait = '208469691';
+var kyrgyzstan = '131031879';
+var laos = '4504696038';
+var lebanon = '246154927';
+var malaysia = '159002621';
+var mongolia = '352911563';
+var myanmar = '537204717';
+var nepal = '58743813';
+var oman = '328359889';
+var pakistan = '218443914';
+var phillipines = '78489442';
+var qatar = '172002218';
+var saudiArabia = '226596397';
+var southKorea = '61426538'; 
+var sriLanka = '91046596';
+var singapore = '454645641';
+var syria = '158658574';
+var tajikstan = '199234302';
+var thailand = '31639623';
+var turkey = '182360370';
+var turkmenistan = '04739099';
+var uae = '148276558';
+var uzbekistan = '292908967';
+
+
+/////EUROPE\\\\\\\\
+
+var albania = '498663728';
+var austria = '86029229';
+var belarus = '2950969565';
+var belgium = '19981036';
+var bosniaHerz = '329542325';
+var bulgaria = '3003327610';
+var croatia = '2900480087';
+var cyprus = '18714842';
+var czech = '82306946';
+var denmark = '19912974';
+var estonia = '18719228';
+var finland = '189056999';
+var france = '72814599';
+var germany = '50965086';
+var greece = '29395463';
+var vatican = '947388787';
+var hungary = '74189049';
+var ireland = '88956373';
+var iceland = '249174648';
+var italy = '36624443';
+var kosovo = '1324616624';
+var latvia = '82436157';
+var lithuania = '2498901252';
+var luxembourg = '833031638';
+var malta = '343949927';
+var moldova = '130804178';
+var montenegro = '2291219215';
+var netherlands = '64403845';
+var northMacedonia = '1229437219';
+var norway = '39764029';
+var poland = '78558366';
+var portugal = '817133173';
+var romania = '1241168023';
+var russia = '20055358';
+var serbia = '17481636';
+var slovakia = '2478328057';
+var slovenia = '59062375';
+var spain = '7616071';
+var sweden = '64954756';
+var switzerland = '127512810';
+var ukraine = '311899155';
+var uk = '14856951';
+
+///////Oceanica\\\\\\\
+
+var australia = '67232748';
+var fiji = '1902330746';
+var micronesia = '589708363';
+var nz = '35617996';
+var papuaNG = '402321115';
+var samoa = '108508328'; 
+
