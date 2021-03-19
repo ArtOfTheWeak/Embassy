9be7eb13dfa29f8411dee4520c90588fe09e3a6d
ArtOfTheWeak <80341310+ArtOfTheWeak@users.noreply.github.com>
2021-03-19 18:19:46 +0000

    Add files via upload

---
+++ 004 russianEmbassylist.js
@@ -0,0 +1,125 @@
+//A list of 97 Russian Embassies worldwide that operate a twitter account and their respective twitter IDs 
+// Based on wikipedia list of Russian  Diplomatic missions: 
+//https://en.wikipedia.org/wiki/List_of_diplomatic_missions_of_Russia
+//Twitter IDS from www.tweeterid.com
+//If you want to get handles, just put IDs into tweeterID.com
+
+
+// AFRICA 
+
+var egypt = '1217887332'; //twitter ID of Russia Embassy in Egypt 
+var eritrea = '898505259915653120'; //twitter ID of Russia Embassy in Eritrea 
+var ghana  = '342052613';
+var guineaSierra = '2216232204'; //twitter ID of Russia Embassy in Guinea and Sierra Leone
+var kenya = '1174451010'; //twitter ID of Russia Embassy in Kenya 
+var mauritius = '2177916206'; //twitter ID of Russia Embassy in Mauritius  
+var namibia = '967711162040274945'; //twitter ID of Russia Embassy in Mauritius  
+var nigeria = '289259276'; //twitter ID of Russia Embassy in Nigeria
+var rwanda =  '2973689249'; //twitter ID of Russia Embassy in rwanda
+var senegal = '2201040799'; //twitter ID of Russia Embassy in senegal
+var seychelles = '1026681586614501376'; //twitter ID of Russia Embassy in Seychelles 
+var sa = '706646642'; //twitter ID of Russia Embassy in South Africa 
+var sudan = '2230446228'; //twitter ID of Russia Embassy in Sudan 
+var tanzania = '2872855836'; //twitter ID of Russia Embassy in tanzania 
+var tunisia = '1092496052'; //twitter ID of Russia Embassy in tunisia 
+var uganda = '2174261359'; //twitter ID of Russia Embassy in uganda 
+var zimbabwe = '2150757894'; //twitter ID of Russia Embassy in zimbabwe
+
+
+//Americas 
+
+var argentina = '340383579';
+var brazil = '243632239';
+var chile = '1060466071424122880';
+var canada = '475954041';
+var colombia = '1969240051';
+var costaRica = '2786944454';
+var cuba = '2765405792';
+var ecuador = '1209375380';
+var guyan = '430703645';
+var jamaica = '249125650';
+var mexico = '1219583316';
+var panama = '714243236055764992';
+var paraguay = '1036734792556011520';
+var peru = '1084150796';
+var USA = '1267887043';
+var uruguay = '2166975690';
+var venezuela = '2154112404';
+
+
+//Asia 
+
+var afghanistan = '341458109';
+var armenia = '841547755449417728';
+var azerbijan = '897998810';
+var bahrain = '2887426436';
+var brunei = '355913325';
+var cambodia = '1168964126';
+var georgia = '250705901';
+var india = '707067944';
+var indonesia = '360374306';
+var iran = '2862427882';
+var iraq = '625136937';
+var israel = '427641057';
+var japan = '1669981711';
+var jordan = '3069186900';
+var kuwait = '1537517570';
+var kyrgyzstan = '2166906422';
+var lebanon = '1175908962';
+var malaysia = '3249337729';
+var mongolia = '832364958';
+var nk = '844532056126312449';
+var oman = '4887336706';
+var pakistan = '754546772';
+var palestine = '2179603751';
+var phillipines = '1003862277982863360';
+var qatar = '1174204969190400000';
+var saudi = '2258679602';
+var singapore = '2258679602';
+var sk = '508086756';
+var sriLanka = '346275669';
+var syria = '2168019920';
+var tajikstan = '702362460';
+var thailand = '398068796';
+var turkey = '848884243';
+var turkmenistan = '1177919665';
+var uae = '700689212';
+var vietnam = '1482255390';
+var yemen = '829080188180905985';
+
+
+//EUROPE
+
+var austria = '3001108415';
+var belarus = '355463027';
+var belgium = '337801457';
+var bosniaher = '3384283787';
+var croatia = '2870280371';
+var denmark = '19649914';
+var estonia = '861038390';
+var france = '342709703';
+var germany = '596243227';
+var greece = '340891133';
+var vatican = '1019971247403421697';
+var hungary = '1222310623';
+var iceland = '2181931022';
+var ireland = '343390384';
+var italy = '508588196';
+var latvia = '1016863818';
+var luxembourg = '347817121';
+var malta = '2560789458';
+var montenegro = '2908230551';
+var netherlands = '256927757';
+var norway = '1034858136';
+var poland = '111219262';
+var serbia = '111219262';
+var slovenia = '429872186';
+var spain = '343933160';
+var sweden = '2863695419 ';
+var swiss = '1200248072';
+var uk = '114718372';
+
+//Oceania 
+
+var australia = '2393592282';
+var nz = '851211924008194048';
